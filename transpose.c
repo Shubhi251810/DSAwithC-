b54d2600ecf5972bf44cebc9ff34a50fbc36542a
Shubhi251810 <shubhichaudhary822@gmail.com>
2025-04-02 12:18:54 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	
+		int arr1[n][n];//arr2[n][n],res[n][n];
+	printf("enter the element of array1: ");
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&arr1[i][j]);
+		}
+	}
+	/*printf("enter the element of array2: ");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&arr2[i][j]);
+		}
+	}*/
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			int temp = arr1[i][j];
+			arr1[i][j]=arr1[j][i];
+			arr1[j][i]=temp;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+	printf("%d",arr1[j][i]);
+}
+printf("\n");
+}
+	return 0;
+}
